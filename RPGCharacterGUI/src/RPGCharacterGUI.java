0a1c1da030b981ab255a3e18ac84cbd558df9895
DJ Ward <warddam001@warren.k12.in.us>
2021-11-06 15:25:44 -0400

    projects 2

--- RPGCharacterGUI/src/RPGCharacterGUI.java
+++ RPGCharacterGUI/src/RPGCharacterGUI.java
@@ -17,27 +17,36 @@ public class RPGCharacterGUI {
         JTextField strengthText = new JTextField(10);
 
 
-        JFrame frame = new
-                JFrame("RPG Character");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JFrame frame = new JFrame("RPG Character");
+
         frame.setSize(350, 350);
+        frame.setLocation(100, 150);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JLabel label1 = new JLabel(" Character Strength");
+
         JButton button = new
                 JButton("Strength Ignite");
+        frame.add(button);
         JButton button1 = new
                 JButton("Energy Ignite");
+        frame.add(button1);
         JButton button2 = new
                 JButton("Dexterity Ignite");
+        frame.add(button2);
         JButton button3 = new
                 JButton("Spirit Ignite");
+        frame.add(button3);
+    //add elements
 
 
 
-        pane.add(button);
-        pane.add(button1);
-        pane.add(button2);
-        pane.add(button3);
 
-        pane.setVisible(true);
+
+        frame.add(label1);
+        frame.add(pane);
+
+        frame.setVisible(true);
+
 
     }}
 
