2cb3b2e133fcffaa7ab46366b563af71c1e45712
DJ Ward <warddam001@warren.k12.in.us>
2021-11-06 11:47:19 -0400

    projects 2

--- RPGCharacterGUI/src/RPGCharacterGUI.java
+++ RPGCharacterGUI/src/RPGCharacterGUI.java
@@ -1,51 +1,46 @@
-
-    import javax.swing.*;
-        import java.awt.*;
-    import java.awt.event.ActionEvent;
-    import java.awt.event.ActionListener;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
 
 
 
 public class RPGCharacterGUI {
-        public static JTextField inputNumber2 = new JTextField("");
-        public static JTextField inputNumber = new JTextField("");
-        public static JLabel finalLabel = new JLabel("");
-static JFrame f;
-static JLabel l;
 
-{
-
-}
 
     public static void main(String[] args) {
-            JLabel numberLabel = new JLabel("Number input: ");
-            JLabel numberLabel2 = new JLabel("Number input 1");
-f = new JFrame("label");
-l = new JLabel();
-l.setText("label text");
 
-JPanel panel = new JPanel();
-panel.add(l);
-f.add(panel);
-f.setSize(325, 352);
-f.show();
 
-        JButton xButt = new JButton("x");
-        JButton subButt = new JButton("-");
-        JButton addButt = new JButton("+");
-        JButton divButt = new JButton("/");
+        JPanel pane = new JPanel();
+        JLabel label = new JLabel ("Enter Strength:");
 
-        panel.add(inputNumber);
-        panel.add(xButt);
-        panel.add(divButt);
-        panel.add(subButt);
-        panel.add(addButt);
+        JTextField strengthText = new JTextField(10);
 
-        String name;
-        String input;
-        int strength;
-        int energy;
 
-    }
-}
+        JFrame frame = new
+                JFrame("RPG Character");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(350, 350);
+        JButton button = new
+                JButton("Strength Ignite");
+        JButton button1 = new
+                JButton("Energy Ignite");
+        JButton button2 = new
+                JButton("Dexterity Ignite");
+        JButton button3 = new
+                JButton("Spirit Ignite");
+
+
+
+        pane.add(button);
+        pane.add(button1);
+        pane.add(button2);
+        pane.add(button3);
+
+        pane.setVisible(true);
+
+    }}
+
+
+
+
