e741e3f88d30197ad7bc187dbb34d1f17557045c
DJ Ward <warddam001@warren.k12.in.us>
2021-11-06 17:01:48 -0400

    projects 2

--- RPGCharacterGUI/src/RPGCharacterGUI.java
+++ RPGCharacterGUI/src/RPGCharacterGUI.java
@@ -1,7 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-
+import java.awt.event.ActionListener;
 
 
 
@@ -10,12 +10,27 @@ public class RPGCharacterGUI {
 
     public static void main(String[] args) {
 
+            int strength;
+            int energy;
+            int dexterity;
+            int spirit;
 
+
+        JTextField input = new JTextField();
         JPanel pane = new JPanel();
         JLabel label = new JLabel ("Enter Strength:");
 
-        JTextField strengthText = new JTextField(10);
+        JTextField strengthText = new JTextField(5);
+              strengthText.setBounds(200, 50, 125, 25);
 
+        JTextField energyText = new JTextField(5);
+        energyText.setBounds(200, 75, 125, 25);
+
+        JTextField dexterityText = new JTextField(5);
+        dexterityText.setBounds(200, 100, 125, 25);
+
+        JTextField spiritText = new JTextField(5);
+         spiritText.setBounds(200, 125, 125, 25);
 
         JFrame frame = new JFrame("RPG Character");
 
@@ -23,26 +38,36 @@ public class RPGCharacterGUI {
         frame.setLocation(100, 150);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         JLabel label1 = new JLabel(" Character Strength");
+        label1.setBounds(50,50,200,30);
 
-        JButton button = new
-                JButton("Strength Ignite");
-        frame.add(button);
-        JButton button1 = new
-                JButton("Energy Ignite");
-        frame.add(button1);
-        JButton button2 = new
-                JButton("Dexterity Ignite");
-        frame.add(button2);
-        JButton button3 = new
-                JButton("Spirit Ignite");
-        frame.add(button3);
+        JLabel label2 = new JLabel(" Character Energy");
+        label2.setBounds(50,75,250,35);
+
+        JLabel label3 = new JLabel(" Character Dexterity");
+        label3.setBounds(50,100,250,35);
+
+        JLabel label4 = new JLabel(" Character Spirit");
+        label4.setBounds(50,125,250,35);
+
+
+
+
+        JButton button4 = new
+                JButton("Ignite Calculate");
+        button4.setBounds(200, 200, 125, 25);
+        frame.add(button4);
     //add elements
 
 
 
-
-
+        frame.add(strengthText);
+        frame.add(energyText);
+        frame.add(dexterityText);
+        frame.add(spiritText);
         frame.add(label1);
+        frame.add(label2);
+        frame.add(label3);
+        frame.add(label4);
         frame.add(pane);
 
         frame.setVisible(true);
